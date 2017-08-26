c50f87ba6b8f23f78f52f708fc65c65b5735a083
franciscoquinones <30478900+franciscoquinones@users.noreply.github.com>
2017-08-26 11:05:42 -0500

    Update adq_mpu6050_leer.py

--- adq_mpu6050_leer.py
+++ adq_mpu6050_leer.py
@@ -22,30 +22,7 @@ def  acelerometro(data1):
     salida[1]=round(float(data[1])*9.81/16384,2)
     salida[2]=round(float(data[2])*9.81/16384,2)
     return salida
-"""    
-def giroscopio(data):
-  global gx,gy,gz
-  print data
-  salida=[0,0]
-  ay=np.arctan2(float(data[1]),np.sqrt(float(data[2])**2+float(data[3])**2)) *180/np.pi
  
-  ax=np.arctan2(float(data[1]),np.sqrt(float(data[1])**2+float(data[3])**2))*180/np.pi        
-  
-  gx = gx + float(data[4]) / 30.0
-  gy = gy - float(data[5]) / 30.0
-  gz = gz + float(data[6]) / 30.0
-
- 
- 
-  gx = gx * 0.96 + ax * 0.04
-  
-  gy = gy * 0.96 + ay * 0.04
-   
-  salida[0]=gx
-  salida[1]=gy
-  
-  return salida
-"""  
 for i in range(15): #se repetira 100 veces
     comuni.write('x')
     data=comuni.readline()
