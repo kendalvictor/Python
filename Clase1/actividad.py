dc3915f29dfdee416501816e9cdbbe43e064422c
franciscoquinones <30478900+franciscoquinones@users.noreply.github.com>
2017-08-29 05:55:11 -0500

    Actividades a Desarrollar

---
+++ Clase1/actividad.py
@@ -0,0 +1,24 @@
+ 
+
+#%%% Ingrese un numero y un divisor mostrar en pantalla si el numero es multiplo de 6, par o impar,tambien debe mostrar si es divisble por el divisor
+  
+    
+#%% REalice un promgrama al ingresar un dato numerico , devuelva un texto especificando si el numero es par o impar
+    
+    
+    
+#%% Realice un promgrama que al ingresar un numero(nota),el numero ingresado debe estar comprendido entre 0-20 (de lo contrario mostrar error),
+#  si el numero ingresado es mayor a 11 devolver, usted ha aprobado , de lo contrario mostrar "usted ha desaprobado".
+    
+#%% Ingresar un numero e impirmirlo en pantalla como texto, imprimir su tipo de dato
+    
+    
+#%% Ingresar 3 numeros  mostrar el numero mayor y eliminar las variables usadas.
+    
+
+#%% Emprimir en pantalla los 50 primeros numeros de la serie de numeros cuadrados(1,4,9,25,etc)
+
+#%%  Mostrar todos los numeros primos solo en el intervalo ingresado,dichos numeros admisibles deben mostrarse de forma intercalda
+#ejemplo intervalo 2 -11, resultado(previo) = 2 3 5 7 11,resultado(mostrado en consola)= 2 5 11      
+    
+    
