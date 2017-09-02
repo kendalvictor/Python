5a615c5656330ff040a546a8e4a74b064c75f743
franciscoquinones <30478900+franciscoquinones@users.noreply.github.com>
2017-09-02 12:41:36 -0500

    Add files via upload

---
+++ Clase2/Ejemplos/ejem 1.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+ 
+
+@author: Francixco
+"""
+
+print "hola"
+#listas append 
+#Agrupa elementos ordenados
+#%%
+lista=[1,2,3,4,5] #LISTA
+print lista
+lista=["hola","mundo","python","one",4]
+print lista
+print len(lista)
+print type(lista) #muestra el tipo de dato
+#%%concatenar
+text1=[1 ,2,3,4,5]#LISTA 1
+text2=[10,9,8,7,6]#LISTA 2
+sal=text1+text2   #CONCATENACION DE LISTAS
+
+print sal[6]      #MOSTRAMOS UN VALOR
+ 
+ 
+ 
+#%%
+#seleccionamos los elemntos de la lista
+text1=[1 ,2,3,4,5]#LISTA 1
+text2=[10,9,8,7,6]#LISTA 2
+print lista[-1]
+
+
+
+
+#%%
+#Porcion de una lista
+text1=[1 ,2,3,4,5]#LISTA 1
+text2=[10,9,8,7,6]#LISTA 2
+ 
+print  text1[1:3]
+print  text1[:3]
+print  text1[3:]
+
+
+
+#%%del borrar una variable
+text1=[1 ,2,3,4,5]#LISTA 1
+text2=[10,9,8,7,6]#LISTA 2
+del text2[1:3]
+print text2 
+
+
+
+
+#%%Existe?
+
+text1=[1 ,2,3,4,5]#LISTA 1
+text2=[10,9,8,7,6]#LISTA 2
+8 in text1
+
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
